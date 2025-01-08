cffc05398656514721490cc5565bf7144a3f3b82
mileshinch <miles.hinchliffe@levellingup.gov.uk>
2025-01-08 18:18:10 +0000

    Do instructive exercise on mapping objects
    Use map array method to map array to array of objects.

--- array-method-guide.js
+++ array-method-guide.js
@@ -38,3 +38,41 @@ function sortDecreasing(arr) {
 
 // copy and sort array of strings
 // keep original array unmodified
+
+function copySorted(arr) {
+    // slice() creates a copy of the array
+    // then sort it
+    return arr.slice().sort();
+}
+
+// You have an array of user objects, each one has user.name.
+// Write the code that converts it into an array of names.
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 28 };
+
+// let users = [ john, pete, mary ];
+
+// let names = users.map(item => item.name)
+
+// You have an array of user objects, each one has name, surname and id.
+// Write the code to create another array from it, of objects with id and fullName, 
+// where fullName is generated from name and surname.
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
+
+let users = [ john, pete, mary ];
+
+// look through users
+// turn each user into an object with id and full name
+let usersMapped = users.map(getIdAndFullname)
+
+function getIdAndFullname(item) {
+    let obj = {id:"", fullname:""}
+    obj.id = item.id
+    obj.fullname = item.name + " " + item.surname
+    return obj
+}
+
+
