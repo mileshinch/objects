8f4e62e076b7d46de819bddca6bc6e0421408523
mileshinch <miles.hinchliffe@levellingup.gov.uk>
2025-01-09 17:23:18 +0000

    Finish array method exercises
    Something clicked today about working with array methods, particularly
    with using map. I wrote the unique function and averageAge function in
    one attempt without any experimenting which I have not done before in
    javascript.

--- array-method-guide.js
+++ array-method-guide.js
@@ -58,21 +58,103 @@ function copySorted(arr) {
 // You have an array of user objects, each one has name, surname and id.
 // Write the code to create another array from it, of objects with id and fullName, 
 // where fullName is generated from name and surname.
-let john = { name: "John", surname: "Smith", id: 1 };
-let pete = { name: "Pete", surname: "Hunt", id: 2 };
-let mary = { name: "Mary", surname: "Key", id: 3 };
+// let john = { name: "John", surname: "Smith", id: 1 };
+// let pete = { name: "Pete", surname: "Hunt", id: 2 };
+// let mary = { name: "Mary", surname: "Key", id: 3 };
 
-let users = [ john, pete, mary ];
+// let users = [ john, pete, mary ];
 
 // look through users
 // turn each user into an object with id and full name
-let usersMapped = users.map(getIdAndFullname)
+// let usersMapped = users.map(getIdAndFullname)
 
-function getIdAndFullname(item) {
-    let obj = {id:"", fullname:""}
-    obj.id = item.id
-    obj.fullname = item.name + " " + item.surname
-    return obj
+// function getIdAndFullname(item) {
+//     let obj = {id:"", fullname:""}
+//     obj.id = item.id
+//     obj.fullname = item.name + " " + item.surname
+//     return obj
+// }
+
+//Write the function sortByAge(users) that gets an array of objects
+// with the age property and sorts them by age.
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 28 };
+
+// let arr = [ pete, john, mary ];
+
+function sortByAge(users) {
+    users.age.sort((x,y) => x - y)
 }
 
+// alert(arr[0].name); // John
+// alert(arr[1].name); // Mary
+// alert(arr[2].name); // Pete
 
+// Write the function shuffle(array) 
+// that shuffles (randomly reorders) elements of the array.
+
+function shuffle(arr) {
+    console.log(arr)
+    // map each element of arr to an array of length 2
+    // made up of the original elemenets + a random number
+    let twoDArray = arr.map(element => [element, Math.random()])
+    console.log(twoDArray)
+    // sort array by math random
+    let sortedTwoDArray = twoDArray.sort((a,b) => a[1] - b[1])
+    console.log(sortedTwoDArray)
+    // map first values of sub arrays to new array
+    let shuffledArray = sortedTwoDArray.map(subArray => subArray[0])
+    return shuffledArray
+
+}
+
+// Write the function getAverageAge(users) 
+// that gets an array of objects with property age and returns the average age
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 29 };
+
+let arr = [ john, pete, mary ];
+
+function getAverageAge(users) {
+    // get an array of ages
+    let arrayOfAges = users.map(element => element.age)
+    console.log(arrayOfAges)
+    // reduce the array to a single number - the sum
+    let sumOfAges = arrayOfAges.reduce((sum, current) => sum + current, 0)
+    console.log(sumOfAges)
+    // divide by length of array for the average
+    let averageAge = sumOfAges / arrayOfAges.length
+    return averageAge  
+}
+
+// let arr be an array
+// write function that returs an array with unique items of arr
+
+let strings = ["Hare", "Krishna", "Hare", "Krishna",
+  "Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
+
+function unique(arr) {
+    // iterate through array
+    for (let i = 0; i < arr.length; i++) {
+        // check if the element appears in a later position of the array
+        if (arr.includes( arr[i], i + 1)) {
+            // if it does then remove that element and decrease i
+            arr.splice( i, 1)
+            i--
+        } else {
+            // if it doesn't then move to the next element
+            continue;
+        }
+    }
+    return arr
+        
+}
+
+// Let’s say we received an array of users in the form {id:..., name:..., age:... }.
+
+// Create a function groupById(arr) that creates an object from it, 
+// with id as the key, and array items as values.
